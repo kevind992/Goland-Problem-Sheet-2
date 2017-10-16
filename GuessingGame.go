51ee1acbcbb862ae611ffa0e52957874e43391e0
Kevin Delassus <g00270791@gmit.ie>
2017-10-16 20:29:18 +0100

    Created File and comments added

---
+++ GuessingGame.go
@@ -0,0 +1,7 @@
+//Author: Kevin Delassus
+//Problem Sheet 2
+//This problem set is for you to learn the fundamentals of creating a web application in Go. 
+//Create a single Git repository as your submission, complete with README and gitignore files. 
+//NB: after completing each exercise, commit your code - there should be at least one commit per exercise. 
+//dYou be will required to submit a URL to the repository and the use of GitHub is recommended for this purpose. 
+//All code should be fully commented, and the README should explain how to clone your repository and run the code.
