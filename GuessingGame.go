f4c9e1e969a0379ff140b7f363227cfbd446dbc6
Kevin Delassus <g00270791@gmit.ie>
2017-10-20 21:09:03 +0100

    Part 3 Complete

--- GuessingGame.go
+++ GuessingGame.go
@@ -9,33 +9,28 @@
 package main
 
 import (
-
   "html/template"
   "log"
   "net/http"
-  
 )
-
 
 type PageVariables struct {
 	Title string
 }
 
 func main() {
-
 	http.HandleFunc("/", HomePage)
-  log.Fatal(http.ListenAndServe(":8080", nil))
-  
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request){
 
-    title := "Guessing Game"
+    title := "Guessing Tom"
     HomePageVars := PageVariables{ //store the title in a struct
       Title : title,
     }
 
-    t, err := template.ParseFiles("Q3.html") //parse the html file homepage.html
+    t, err := template.ParseFiles("template1.html") //parse the html file homepage.html
     if err != nil { // if there is an error
   	  log.Print("template parsing error: ", err) // log it
   	}
