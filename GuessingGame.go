c27b32c6da6544ca8d66de39f7988f0fd5cd2003
Kevin Delassus <g00270791@gmit.ie>
2017-10-20 20:18:52 +0100

    Part 2 Complete

--- GuessingGame.go
+++ GuessingGame.go
@@ -10,41 +10,12 @@ package main
 
 import (
 
-  //"fmt"
   "html/template"
   "log"
   "net/http"
-  //"bytes"
+  
 )
-/*func requestHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "r.Method:           ",  r.Method           )
-	fmt.Fprintln(w, "r.URL:              ",  r.URL              )
-	fmt.Fprintln(w, "r.Proto:            ",  r.Proto            )
-	fmt.Fprintln(w, "r.ContentLength:    ",  r.ContentLength    )
-	fmt.Fprintln(w, "r.TransferEncoding: ",  r.TransferEncoding )
-	fmt.Fprintln(w, "r.Close:            ",  r.Close            )
-	fmt.Fprintln(w, "r.Host:             ",  r.Host             )
-	fmt.Fprintln(w, "r.Form:             ",  r.Form             )
-	fmt.Fprintln(w, "r.PostForm:         ",  r.PostForm         )
-	fmt.Fprintln(w, "r.RemoteAddr:       ",  r.RemoteAddr       )
-	fmt.Fprintln(w, "r.RequestURI:       ",  r.RequestURI       )
 
-	fmt.Fprintln(w, "r.URL.Opaque:       ", r.URL.Opaque        )
-	fmt.Fprintln(w, "r.URL.Scheme:       ", r.URL.Scheme        )
-	fmt.Fprintln(w, "r.URL.Host:         ", r.URL.Host          )
-	fmt.Fprintln(w, "r.URL.Path:         ", r.URL.Path          )
-	fmt.Fprintln(w, "r.URL.RawPath:      ", r.URL.RawPath       )
-	fmt.Fprintln(w, "r.URL.RawQuery:     ", r.URL.RawQuery      )
-	fmt.Fprintln(w, "r.URL.Fragment:     ", r.URL.Fragment      )
-
-	for key, value := range r.Header {
-		fmt.Fprintln(w, "\t" + key + ":", value)
-	}
-
-	body := new(bytes.Buffer)
-	body.ReadFrom(r.Body)
-	fmt.Fprintln(w, "r.Body:             ",  body.String())
-}*/
 
 type PageVariables struct {
 	Title string
@@ -52,9 +23,9 @@ type PageVariables struct {
 
 func main() {
 
-  //http.HandleFunc("/", requestHandler)
 	http.HandleFunc("/", HomePage)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+  log.Fatal(http.ListenAndServe(":8080", nil))
+  
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request){
